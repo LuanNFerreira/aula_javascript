766235186e79b98e972dbebf3f0138692a10078c
LuanDante <luan.nascimentoferreira@gmail.com>
2020-06-27 23:09:33 -0300

    alterando arquivos de exemplos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
 
-var nome = "Luan do N. Ferreira";
-var idade = "23";
-alert(nome + " tem " + idade + " anos");
-console.log(nome);
-console.log(idade);
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome)
+
+
+/*var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome)
+*/
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva"); //push para adicionar
+//lista.pop(); //pop para tirar o ultimo elemento
+//console.log(lista);
+
+//console.log(lista.length); //mostra quantidade da lista
+//console.log(lista.reverse()); //mostra ao contrario a lista
+//console.log(lista.toString()); //imprimi como string
+//console.log(lista.join(" - ")) //adiciona um espaçamento especifico
+//alert(lista[1]); alerta de lista
+
+//var nome = "Luan do N. Ferreira";
+//var idade = "23"; //"" transforma o numero em string 
+//var n1 = 10; // sem "" vira numero inteiro
+//var n2 = 5;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+
+//isto é uma anotação;
+
+//console.log(nome);
+//console.log(idade);
+//console.log(n1 + n2);
+//console.log(frase.replace("Japão", "Brasil")); serve para trocar uma frase
+//console.log(frase.toUpperCase()); //tudo em LETRA MAIUSCULA
+//console.log(frase.toLowerCase()); //tydo em letra minuscula
+//alert(frase.replace("Japão", "Brasil"));
